83e279c275f01fcde6faab09471363256fac89a9
Robin Laevaert <robinlaevaert1@telenet.be>
2019-12-05 21:00:21 +0100

    Setup Day6

---
+++ AdventOfCode/AdventOfCode/Day 6/Day6.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Day_6
+{
+    public class Day6 : Day
+    {
+        public Day6()
+        {
+            Title = "";
+            dayNumber = 6;
+        }
+        public override void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Part2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
